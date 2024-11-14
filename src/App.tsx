ad8183f2590a892f83c405a5cea42f097659691f
Anastasiya <shirochina16@gmail.com>
2024-11-14 18:45:21 +0100

    Deliete SocketContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import LoginPage from './components/authPage/LoginPage.tsx';
 import MainPage from './components/mainPage/MainPage.tsx';
 import Game from './components/gamePage/game/Game.tsx';
 import { Route, BrowserRouter, Routes} from 'react-router-dom';
-import { SocketProvider } from './SocketContext.tsx';  
 
 
 function App() {
   return (
     <div className='app'>
       <BrowserRouter>
-        <SocketProvider>
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/main" element={<MainPage />} />
             <Route path="/game/:id" element={<Game />} />
           </Routes>
-        </SocketProvider>
       </BrowserRouter>
     </div>
   );
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Games {
+    gameId: string;
+    player_x: string;
+    player_o: string;
+    x: number[];
+    o: number[];
+}
